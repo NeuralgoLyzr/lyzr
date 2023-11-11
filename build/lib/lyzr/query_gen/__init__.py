962410aaeaa09f24b1b37a4be81b3c703cc64767
patel <khush@lyzr.ai>
2023-11-11 16:10:12 +0300

    build:

---
+++ build/lib/lyzr/query_gen/__init__.py
@@ -0,0 +1,3 @@
+from lyzr.query_gen.analysis_query_generator import get_analysis_queries
+
+__all__ = ["get_analysis_queries"]
--- cookbook/chatbot/chat_from_pdf.py
+++ cookbook/chatbot/chat_from_pdf.py
@@ -1,19 +1,15 @@
 from pprint import pprint
 
 import openai
-
 from lyzr import ChatBot
 
 openai.api_key = "sk-"
-
 path = ""
 
-rag = ChatBot.pdf_chat(
+qa_bot = ChatBot.pdf_chat(
+    input_files=[path],
     llm_params={"model": "gpt-3.5-turbo"},
 )
-
 _query = ""
-
-rag = rag.chat(_query)
-
-pprint(rag.response)
+res = qa_bot.chat(_query)
+print(res.response)
